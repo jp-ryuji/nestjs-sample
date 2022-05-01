610bb989e37821db8aebcc3891c4200f0432c18b
Ryuji Eguchi <jp.ryuji@gmail.com>
2022-05-01 15:12:57 +0900

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
@@ -11,6 +12,17 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true })
   )
+
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('nest')
+      .setDescription('NestJS Endpoint List')
+      .setVersion('0.0.1')
+      .build()
+    const document = SwaggerModule.createDocument(app, options)
+    SwaggerModule.setup('api', app, document)
+  }
+
   await app.listen(3000)
 }
 bootstrap()
