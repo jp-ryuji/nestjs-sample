49e488078d497363e37bd78e28157dbf796bb952
Ryuji Eguchi <jp.ryuji@gmail.com>
2022-05-01 14:07:06 +0900

    update main.ts to work with fastify - `yarn start` works - `curl -s http://localhost:3000/` works

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify/adapters/fastify-adapter';
+import { NestFastifyApplication } from '@nestjs/platform-fastify/interfaces/nest-fastify-application.interface';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(3000);
 }
 bootstrap();
