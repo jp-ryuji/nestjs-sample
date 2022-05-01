dc7431ba677b4dcafc4455b06d02ec7ad1932a0e
Ryuji Eguchi <jp.ryuji@gmail.com>
2022-05-01 14:24:22 +0900

    fix app.e2e-spec.ts to work with fastify

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
-import { AppModule } from './../src/app.module'
+
+import { FastifyAdapter } from '@nestjs/platform-fastify/adapters'
+import { NestFastifyApplication } from '@nestjs/platform-fastify/interfaces'
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { AppModule } from '../src/app.module'
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication
+  let app: NestFastifyApplication
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
 
-    app = moduleFixture.createNestApplication()
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter()
+    )
     await app.init()
+    await app.getHttpAdapter().getInstance().ready()
   })
 
   it('/ (GET)', () => {
