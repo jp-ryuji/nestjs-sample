12c835d048d87fa4682cab65f1e3978546edd4a6
Ryuji Eguchi <jp.ryuji@gmail.com>
2022-05-01 14:15:31 +0900

    fix styles with yarn lint

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter } from '@nestjs/platform-fastify/adapters/fastify-adapter';
-import { NestFastifyApplication } from '@nestjs/platform-fastify/interfaces/nest-fastify-application.interface';
+import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter } from '@nestjs/platform-fastify/adapters/fastify-adapter'
+import { NestFastifyApplication } from '@nestjs/platform-fastify/interfaces/nest-fastify-application.interface'
 
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(3000);
+    new FastifyAdapter()
+  )
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
